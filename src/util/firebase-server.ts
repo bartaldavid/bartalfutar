3c852cca9cccd9f22ac1d175e0834dbd697d30d3
Bartal David <bartaldvd@gmail.com>
2023-04-25 21:50:28 +0200

    back to commonjs

--- src/util/firebase-server.ts
+++ src/util/firebase-server.ts
@@ -1,21 +1,14 @@
-import {
-	initializeApp,
-	auth,
-	apps,
-	type ServiceAccount,
-	credential,
-	firestore
-} from 'firebase-admin';
+import firebaseAdmin from 'firebase-admin';
 import type { savedStop } from './client/savedStop';
 import { FIREBASE_SERVICE_ACCOUNT_KEY } from '$env/static/private';
 
-const serviceAccount: ServiceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY);
+const serviceAccount: firebaseAdmin.ServiceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY);
 
 function initializeFirebase() {
-	if (apps.length) return;
+	if (firebaseAdmin.apps.length) return;
 
-	initializeApp({
-		credential: credential.cert(serviceAccount)
+	firebaseAdmin.initializeApp({
+		credential: firebaseAdmin.credential.cert(serviceAccount)
 	});
 }
 
@@ -23,7 +16,7 @@ export async function decodeToken(token: string) {
 	if (!token) return null;
 	try {
 		initializeFirebase();
-		return await auth().verifyIdToken(token);
+		return await firebaseAdmin.auth().verifyIdToken(token);
 	} catch (err) {
 		console.log(err);
 		return null;
@@ -34,7 +27,7 @@ export async function getUserData(uid: string): Promise<savedStop[]> {
 	if (!uid) return [];
 	try {
 		initializeFirebase();
-		const db = firestore();
+		const db = firebaseAdmin.firestore();
 		const querySnapshot = await db.collection(`userdata/${uid}/stops`).get();
 		const stops = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 		return stops;
