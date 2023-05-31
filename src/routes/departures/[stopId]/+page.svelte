872215ed52314d2675de0f93435bca99b1398cdb
David Bartal <bartaldvd@gmail.com>
2023-05-31 16:56:50 +0200

    loading indicator in stop name

--- src/routes/departures/[stopId]/+page.svelte
+++ src/routes/departures/[stopId]/+page.svelte
@@ -19,7 +19,7 @@
 	class="flex sm:h-[calc(100vh-2.5rem)] w-full flex-col gap-2 sm:pr-4 pt-4 sm:w-72 sm:overflow-auto"
 >
 	<div class="flex gap-2 dark:text-slate-100 justify-between pb-2">
-		<h1 class="text-lg self-baseline">{$stopData.data?.references?.stops?.[data.stopId]?.name}</h1>
+		<h1 class="text-lg self-baseline">{$stopData.data?.references?.stops?.[data.stopId]?.name ?? "Loading..."}</h1>
 		<div class="flex items-center">
 			<button class="px-2" on:click={async () => await $stopData.refetch()}
 				><span class="material-symbols-outlined text-base">
