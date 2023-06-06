f57c61e3e3ac736ce6cfbcaacdd17afeebde2e09
David Bartal <bartaldvd@gmail.com>
2023-06-07 00:03:52 +0200

    api key

--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -6,22 +6,33 @@ import {
 	defaultTripParams
 } from '../data/defaultParams';
 
+// TODO secure api key somehow?
+import { PUBLIC_BKK_API_KEY } from '$env/static/public';
+
+// TODO refractor these to remove repetition
+
 export async function fetchStopDepartures(stopId: string, limit?: number) {
 	const params = { ...defaultStopParams, stopId: stopId, limit: limit ?? 10 };
-	const response = await fetch(stopDataUrl + new URLSearchParams(params as any));
+	const response = await fetch(
+		stopDataUrl + new URLSearchParams({ key: PUBLIC_BKK_API_KEY, ...params } as any)
+	);
 	const data: components['schemas']['ArrivalsAndDeparturesForStopOTPMethodResponse'] =
 		await response.json();
 	return data.data;
 }
 export async function fetchTripDetails(tripId: string) {
 	const params = { ...defaultTripParams, tripId: tripId };
-	const response = await fetch(tripDataUrl + new URLSearchParams(params as any));
+	const response = await fetch(
+		tripDataUrl + new URLSearchParams({ key: PUBLIC_BKK_API_KEY, ...params } as any)
+	);
 	const data: components['schemas']['TripDetailsOTPMethodResponse'] = await response.json();
 	return data.data;
 }
 export async function fetchStopsForQuery(searchQuery: string) {
 	const params = { ...defaultStopsForLocationParams, query: searchQuery };
-	const response = await fetch(stopsForLocationUrl + new URLSearchParams(params as any));
+	const response = await fetch(
+		stopsForLocationUrl + new URLSearchParams({ key: PUBLIC_BKK_API_KEY, ...params } as any)
+	);
 	const data: components['schemas']['StopsForLocationResponse'] = await response.json();
 	return data.data;
 }
