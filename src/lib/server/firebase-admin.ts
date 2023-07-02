e1ffee1b8da2510fd1536dc3783e4a041e2ac9f6
David Bartal <bartaldvd@gmail.com>
2023-07-02 11:09:22 +0200

    refractor admin init

--- src/lib/server/firebase-admin.ts
+++ src/lib/server/firebase-admin.ts
@@ -1,18 +1,18 @@
-import admin from 'firebase-admin';
-import { FIREBASE_SERVICE_ACCOUNT_KEY } from '$env/static/private';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
+import { cert, getApps, initializeApp } from 'firebase-admin/app';
+import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
 
-const serviceAccount: admin.ServiceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY);
+// const serviceAccount: admin.ServiceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY);
 
-function initializeFirebase() {
-	if (admin.apps.length) return admin.app();
-
-	return admin.initializeApp({
-		credential: admin.credential.cert(serviceAccount)
+if (!getApps().length)
+	initializeApp({
+		credential: cert({
+			projectId: FB_PROJECT_ID,
+			clientEmail: FB_CLIENT_EMAIL,
+			privateKey: FB_PRIVATE_KEY
+		})
 	});
-}
 
-export const app = initializeFirebase();
 export const serverAuth = getAuth();
 export const adminDB = getFirestore();
