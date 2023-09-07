787ed72731c0394fc5f27592e2dd79ab9337c489
David Bartal <bartaldvd@gmail.com>
2023-09-07 23:22:27 +0200

    remove outdated code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,33 +25,8 @@
 </svelte:head>
 
 <QueryClientProvider client={data.queryClient}>
-  <!-- <Header /> -->
-  <main class="mx-2 flex justify-center">
-    <!-- {#if $user || data.user}
-      <div
-        class="mt-4 w-full flex-col flex gap-1 {isSarchOpen && 'hidden md:flex'} gap-2 sm:w-72"
-        class:justify-center={!data.stops?.length}
-      >
-        {#if !$savedStops.length && !data.stops?.length}
-          <div class="p-4 pb-0 text-center text-4xl dark:text-slate-50">BartalFUTÁR</div>
-          <div class="pb-10 text-center dark:text-slate-200">Add stops to get started</div>
-        {:else}
-          <StopsView />
-        {/if}
-
-        <a
-          class="p-2 rounded button-outline bg-white items-center justify-center flex dark:border-none dark:bg-slate-700 dark:text-white"
-          href="/search"
-          use:shortcut={{ control: true, code: 'KeyK' }}
-          role="button"
-        >
-          <div class="flex items-center gap-1 justify-center">
-            <Search class="inline" /><span>Search</span>
-          </div>
-        </a>
-      </div>
-    {/if} -->
-    <slot />
+  <main class="mx-2 flex justify-center md:flex-row">
     <NavBar />
+    <slot />
   </main>
 </QueryClientProvider>
