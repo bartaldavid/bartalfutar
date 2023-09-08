f7627517007f768b6e8a5087fb3f78b2b54b2165
David Bartal <bartaldvd@gmail.com>
2023-09-08 13:11:55 +0200

    show location status & fetch on load

--- src/routes/nearby/+page.svelte
+++ src/routes/nearby/+page.svelte
@@ -8,42 +8,58 @@
   import DepartureGroup from '../../components/DepartureGroup.svelte';
   import { loadLocation, location } from '$lib/stores/geolocation';
   import { REFETCH_INTERVAL_MS } from '../../data/constants';
+  import LocationSearchingIcon from "~icons/material-symbols/location-searching";
+  import MyLocationIcon from "~icons/material-symbols/my-location";
+  import LocationDisabledIcon from "~icons/material-symbols/location-disabled"
+  import { error } from '@sveltejs/kit';
 
   $: nearbyDepartures = createQuery({
-    queryKey: ['departuresForLocation'],
+    queryKey: ['departuresForLocation', $location.position?.coords],
     queryFn: async () =>
       safeFetch<components['schemas']['ArrivalsAndDeparturesForLocationOTPMethodResponse']>(
         nearbyDeparturesUrl({
-          lon: 19.1836588361196,
-          lat: 47.452680314157426,
+          lon: $location.position?.coords.longitude,
+          lat: $location.position?.coords.latitude,
           radius: 500,
-          minutesAfter: 90,
+          minutesAfter: 180,
           onlyDepartures: true,
-          limit: 30,
+          limit: 50,
           minResult: 5,
           version: '4'
-          // includeReferences: ['stops']
         })
       ),
-    refetchInterval: REFETCH_INTERVAL_MS
-    // enabled: false
+    refetchInterval: REFETCH_INTERVAL_MS,
+    enabled: $location.isLoaded
   });
 
-  $: console.log($location);
+  onMount(() => {
+    loadLocation();
+  });
 </script>
 
 <PageLayout pageTitle="Around you">
+  <svelte:fragment slot="header">
+    <button
+      class="text-white"
+      on:click={() => {
+        loadLocation();
+      }}>{#if $location.isLoaded}
+          <MyLocationIcon />
+        {:else if $location.error}
+        <LocationDisabledIcon />
+        {:else}
+        <LocationSearchingIcon />
+      {/if}</button
+    >
+  </svelte:fragment>
   <svelte:fragment slot="content">
     <div class="flex flex-col gap-2">
-      <button
-        class="text-white"
-        on:click={() => {
-          loadLocation();
-        }}>get location</button
-      >
       {#each $nearbyDepartures.data?.data?.list ?? [] as departureGroup ((departureGroup.routeId ?? '') + departureGroup.headsign)}
         <DepartureGroup {departureGroup} references={$nearbyDepartures.data?.data?.references} />
       {/each}
+      {#if $location.error}
+        <div class="text-red-500">{$location.error.message}</div>
+      {/if}
     </div>
   </svelte:fragment>
 </PageLayout>
