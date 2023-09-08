efd68a45d2706892ded6ea1f74a3d9baaf4be6ef
David Bartal <bartaldvd@gmail.com>
2023-09-08 12:30:21 +0200

    do not render header if no pagetitle

--- src/components/PageLayout.svelte
+++ src/components/PageLayout.svelte
@@ -3,9 +3,11 @@
 </script>
 
 <div class="mb-24 flex w-full flex-col md:w-72">
-  <div class="mb-4 mt-12 flex items-center justify-between">
-    <h1 class="text-2xl dark:text-white">{pageTitle}</h1>
-    <slot name="header" />
-  </div>
+  {#if pageTitle}    
+    <div class="mb-4 mt-12 flex items-center justify-between">
+      <h1 class="text-xl dark:text-white">{pageTitle}</h1>
+      <slot name="header" />
+    </div>
+  {/if}
   <slot name="content">No content</slot>
 </div>
