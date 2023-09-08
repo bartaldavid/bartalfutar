b90a67c23b2af101c1c8cee567b673fe0069e236
David Bartal <bartaldvd@gmail.com>
2023-09-08 13:10:59 +0200

    redesign group

--- src/components/DepartureGroup.svelte
+++ src/components/DepartureGroup.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { components } from '../data/bkk-openapi';
   import { displayDate, useTransitStopTime } from '../lib/util/date';
+  import Countdown from './Countdown.svelte';
   import RouteIcon from './RouteIcon.svelte';
   import VehicleIcons from './VehicleIcons.svelte';
 
@@ -11,9 +12,12 @@
 </script>
 
 <div class="flex flex-col gap-1 rounded bg-slate-100 p-4 dark:bg-slate-800 dark:text-slate-50">
-  <RouteIcon {routeData} headSign={departureGroup.headsign} />
+  <div class="flex flex-row text-sm my-1 items-center gap-1">
+    <VehicleIcons vehicleType={routeData?.type ?? ''} class="self-center text-lg"/>
+    <RouteIcon {routeData} />
+    <span>{departureGroup.headsign}</span>
+  </div>
   <div class="flex gap-1 text-sm">
-    <VehicleIcons vehicleType={routeData?.type ?? ''} />
     {#if departureGroup.stopTimes}
       {#each departureGroup.stopTimes as stopTime}
         {@const { relevantDate, isDelayed, isRealtime } = useTransitStopTime(stopTime)}
@@ -21,6 +25,9 @@
         <span class:text-red-500={isDelayed} class:text-green-500={isRealtime && !isDelayed}
           >{displayDate(relevantDate)}</span
         >
+        <!-- {#if relevantDate}
+          <Countdown countDownToDate={relevantDate} />
+        {/if} -->
       {/each}
     {:else}
       <span class="text-slate-200">No departure in the next 90 minutes</span>
