ee9c3c5d2c2b0ef0e251666b815bf57f9cd500af
David Bartal <bartaldvd@gmail.com>
2023-09-10 19:15:01 +0200

    format

--- src/routes/favorites/+page.server.ts
+++ src/routes/favorites/+page.server.ts
@@ -2,15 +2,15 @@ import { adminDB } from '$lib/server/firebase-admin';
 import type { savedStop } from '$lib/stores/favorite-stops';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({locals}) => {
+export const load = (async ({ locals }) => {
+  const userId = locals.userId;
 
-    const userId = locals.userId;
+  if (!userId) {
+    return { stops: [] };
+  }
 
-    if (!userId) { return {stops: []}}
+  const querySnapshot = await adminDB.collection(`userdata/${userId}/stops`).get();
+  const stops = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }) as savedStop);
 
-    const querySnapshot = await adminDB.collection(`userdata/${userId}/stops`).get();
-    const stops = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id } as savedStop));
-
-    return {stops};
-
+  return { stops };
 }) satisfies PageServerLoad;
