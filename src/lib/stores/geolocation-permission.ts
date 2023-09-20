989c68b98648805561ad5046246fc3ffcdf45210
David Bartal <bartaldvd@gmail.com>
2023-09-20 21:50:26 +0200

    formatting

--- src/lib/stores/geolocation-permission.ts
+++ src/lib/stores/geolocation-permission.ts
@@ -1,11 +1,13 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
-export const geolocationPermissionState = writable<PermissionState>("prompt");
+export const geolocationPermissionState = writable<PermissionState>('prompt');
 
+// FIXME: This is an alternate solution, but it doesn't work in Safari, maybe refractor this into a store with addeventlistener
 export async function listenForPermissionChange() {
-    if (!navigator) return;
+  if (!navigator) return;
 
-    const geolocationPermission = await navigator.permissions.query({ name: "geolocation" });
-    geolocationPermissionState.set(geolocationPermission.state)
-    geolocationPermission.onchange = () => geolocationPermissionState.set(geolocationPermission.state)
+  const geolocationPermission = await navigator.permissions.query({ name: 'geolocation' });
+  geolocationPermissionState.set(geolocationPermission.state);
+  geolocationPermission.onchange = () =>
+    geolocationPermissionState.set(geolocationPermission.state);
 }
