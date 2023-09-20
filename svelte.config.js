6bd16ce5210e20f8abeea3455d9a7e8a4de4ae5f
David Bartal <bartaldvd@gmail.com>
2023-09-20 08:42:49 +0200

    undo edge

--- svelte.config.js
+++ svelte.config.js
@@ -11,29 +11,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      runtime: 'edge',
-      regions: ['dub1', 'fra1'],
-      external: [
-        'stream',
-        'buffer',
-        'https',
-        'os',
-        'util',
-        'querystring',
-        'http',
-        'url',
-        'events',
-        'zlib',
-        'assert',
-        'child_process',
-        'fs',
-        'path',
-        'net',
-        'crypto',
-        'tls'
-      ]
-    })
+    adapter: adapter()
   }
 };
 
