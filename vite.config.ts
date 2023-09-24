93af1f7e4566dac4fa97c4777c14b15262228cde
Dávid Bartal <35382618+bartaldavid@users.noreply.github.com>
2023-09-24 21:29:17 +0200

    new icons

--- vite.config.ts
+++ vite.config.ts
@@ -15,14 +15,26 @@ export default defineConfig({
         theme_color: '#334155',
         icons: [
           {
-            src: 'android-chrome-192x192.png',
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ],
         display: 'standalone'
