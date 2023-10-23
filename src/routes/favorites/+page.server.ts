891643f7de2cf706294540301ce631b901a8d17c
Dávid Bartal <35382618+bartaldavid@users.noreply.github.com>
2023-10-24 00:23:07 +0200

    mostly working with pscale and drizzle

--- src/routes/favorites/+page.server.ts
+++ src/routes/favorites/+page.server.ts
@@ -1,52 +1,25 @@
-import { favoriteStops, stops } from '$lib/schemas/user-data.js';
+import { favoriteStopsTable, transitStopsTable } from '$lib/schemas/user-data.js';
 import { db } from '$lib/server/db.js';
 import { eq } from 'drizzle-orm';
-
-// FIXME casting type to an enum here is not legit
-type Stop = {
-  id: string;
-  type?:
-    | 'WALK'
-    | 'BICYCLE'
-    | 'CAR'
-    | 'TRAM'
-    | 'SUBWAY'
-    | 'SUBURBAN_RAILWAY'
-    | 'RAIL'
-    | 'COACH'
-    | 'BUS'
-    | 'TROLLEYBUS'
-    | 'FERRY'
-    | 'CABLE_CAR'
-    | 'GONDOLA'
-    | 'FUNICULAR'
-    | 'TRANSIT'
-    | 'TRAINISH'
-    | 'BUSISH'
-    | 'LEG_SWITCH'
-    | 'CUSTOM_MOTOR_VEHICLE';
-  name?: string;
-  locationType?: number;
-};
 
 export async function load({ parent }) {
   const { session } = await parent();
   const userId = session?.user.id;
 
   if (!userId) {
-    return { stops: [] as Stop[] };
+    return { stops: [] };
   }
 
   const result = await db
     .select({
-      id: stops.id,
-      type: stops.type,
-      name: stops.name,
-      locationType: stops.locationType
+      id: transitStopsTable.id,
+      type: transitStopsTable.type,
+      name: transitStopsTable.name,
+      locationType: transitStopsTable.locationType
     })
-    .from(stops)
-    .innerJoin(favoriteStops, eq(stops.id, favoriteStops.stopId))
-    .where(eq(favoriteStops.userId, userId))
+    .from(transitStopsTable)
+    .innerJoin(favoriteStopsTable, eq(transitStopsTable.id, favoriteStopsTable.stopId))
+    .where(eq(favoriteStopsTable.userId, userId))
     .execute();
 
   return { stops: result };
--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -1,6 +1,12 @@
 import { stopsForLocationUrl } from '$lib/data/api-links.js';
 import type { components } from '$lib/data/bkk-openapi';
-import { sql } from '$lib/server/db';
+import {
+  favoriteStopsTable,
+  transitRoutesTable,
+  transitStopsTable
+} from '$lib/schemas/user-data.js';
+import { db, sql } from '$lib/server/db.js';
+import { arrayContains, eq } from 'drizzle-orm';
 
 export async function load({ fetch, url }) {
   const query = url.searchParams.get('q')?.toString();
@@ -8,57 +14,38 @@ export async function load({ fetch, url }) {
 
   if (!query || query.length < 3) {
     const now = performance.now();
-    const { favorite_stops, routes } = await sql.begin(async (sql) => {
-      const favorite_stops = await sql<
-        {
-          id: string;
-          name: string;
-          type: string;
-          locationType: number;
-          direction: string;
-          routeIds: string[];
-        }[]
-      >`
-        select
-          stops.stop_id as id,
-          stops.name,
-          stops.type,
-          stops.location_type as "locationType",
-          stops.direction,
-          stops.route_ids as "routeIds"
-        from
-          stops
-        join favorite_stops on stops.stop_id = favorite_stops.stop_id`;
-      // where favorite_stops.user_id = ${userId}`;
 
-      const [routes] = await sql<
-        {
-          routes: {
-            [key: string]: {
-              id: string;
-              shortName?: string;
-              style?: components['schemas']['TransitRouteStyle'];
-            };
-          };
-        }[]
-      >`
-        SELECT
-          JSONB_OBJECT_AGG(
-          transit_routes.id,
-          JSONB_BUILD_OBJECT(
-            'shortName',
-            transit_routes.short_name,
-            'style',
-            transit_routes.style
-          )
-        ) as routes
-        FROM
-            transit_routes
-        JOIN stops ON stops.route_ids @> ARRAY[transit_routes.id]
-        JOIN favorite_stops ON stops.stop_id = favorite_stops.stop_id`;
-      // WHERE favorite_stops.user_id = ${userId}`;
-      return { favorite_stops, routes };
-    });
+    const favorite_stops_query = db
+      .select({
+        id: transitStopsTable.id,
+        name: transitStopsTable.name,
+        type: transitStopsTable.type,
+        locationType: transitStopsTable.locationType,
+        direction: transitStopsTable.direction,
+        routeIds: transitStopsTable.routeIds
+      })
+      .from(transitStopsTable)
+      .innerJoin(favoriteStopsTable, eq(transitStopsTable.id, favoriteStopsTable.stopId))
+      .where(eq(favoriteStopsTable.userId, 'userId'))
+      .toSQL().sql;
+    const routes_query = db
+      .select({
+        id: transitRoutesTable.id,
+        shortName: transitRoutesTable.shortName,
+        style: transitRoutesTable.style
+      })
+      .from(transitRoutesTable)
+      .innerJoin(
+        transitStopsTable,
+        arrayContains(transitStopsTable.routeIds, transitRoutesTable.id)
+      )
+      .innerJoin(favoriteStopsTable, eq(transitStopsTable.id, favoriteStopsTable.stopId))
+      .where(eq(favoriteStopsTable.userId, 'userId'))
+      .toSQL().sql;
+
+    const result = await sql.transaction([sql`select 1 as num`, sql`select 2 as num`]);
+
+    console.log(result);
 
     const end = performance.now();
     console.log(`Query took ${end - now}ms`);
@@ -66,8 +53,7 @@ export async function load({ fetch, url }) {
     return {
       searchData: {
         data: {
-          list: favorite_stops,
-          references: { routes: routes.routes }
+          list: []
         }
       } as components['schemas']['StopsForLocationResponse'],
       query
