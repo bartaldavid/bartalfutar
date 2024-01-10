2924cc87cea4511974377a4d726b8ec7b08c4bb1
David Bartal <bartaldvd@gmail.com>
2024-01-10 16:11:39 +0100

    update to sveltekit 2

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,8 +4,8 @@ export async function load({ locals }) {
   const session = await locals.getSession();
 
   if (session) {
-    throw redirect(303, '/favorites');
+    redirect(303, '/favorites');
   } else {
-    throw redirect(303, '/nearby');
+    redirect(303, '/nearby');
   }
 }
