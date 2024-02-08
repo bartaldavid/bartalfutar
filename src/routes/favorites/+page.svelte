aa81f6b5c52d9c532ebaf8fcce03c42dc0a0960f
David Bartal <bartaldvd@gmail.com>
2024-02-08 09:51:27 +0100

    add: shadcn, popover

--- src/routes/favorites/+page.svelte
+++ src/routes/favorites/+page.svelte
@@ -2,7 +2,9 @@
   import StopsView from '../../components/StopsView.svelte';
   import PageLayout from '../../components/PageLayout.svelte';
   import type { PageData } from './$types';
+  import * as Avatar from '$lib/components/ui/avatar';
   import { UserCircle } from 'lucide-svelte';
+  import * as Popover from '$lib/components/ui/popover';
 
   export let data: PageData;
   $: stops = data.stops ?? [];
@@ -11,7 +13,7 @@
 <PageLayout pageTitle="Favorites">
   <svelte:fragment slot="header">
     <div class="flex items-center">
-      {#if data.session?.user?.image}
+      <!-- {#if data.session?.user?.image}
         <img
           src={data.session?.user?.image}
           alt="Profile"
@@ -19,15 +21,34 @@
           width="30"
           class="m-1 aspect-square rounded-full"
           loading="lazy"
-        />
-      {:else}
-        <UserCircle size="30" />
-      {/if}
-      {#if data.session}
-        <a href="/auth/signout" data-sveltekit-preload-data="off">Sign Out</a>
-      {:else}
-        <a href="/auth/signin" data-sveltekit-preload-data="off">Sign In</a>
-      {/if}
+        /> -->
+      <Popover.Root>
+        <Popover.Trigger>
+          <Avatar.Root>
+            <Avatar.Image
+              src={data.session?.user?.image}
+              alt="Profile"
+              class="m-1 aspect-square rounded-full"
+              loading="lazy"
+              height="30"
+              width="30"
+            />
+            <Avatar.Fallback>
+              <UserCircle size="30" />
+            </Avatar.Fallback>
+          </Avatar.Root>
+        </Popover.Trigger>
+        <Popover.Content class="w-40">
+          {#if data.session}
+            <a href="/auth/signout" data-sveltekit-preload-data="off">Sign Out</a>
+          {:else}
+            <a href="/auth/signin" data-sveltekit-preload-data="off">Sign In</a>
+          {/if}
+        </Popover.Content>
+      </Popover.Root>
+      <!-- {:else}
+          <UserCircle size="30" />
+          {/if} -->
     </div>
   </svelte:fragment>
 
