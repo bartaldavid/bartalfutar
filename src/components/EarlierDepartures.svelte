23ebe611170af8aa8d3bfdc4bb98cdc6099be347
David Bartal <bartaldvd@gmail.com>
2024-02-19 15:29:58 +0100

    fix light mode earlier dep background

--- src/components/EarlierDepartures.svelte
+++ src/components/EarlierDepartures.svelte
@@ -8,7 +8,9 @@
 </script>
 
 {#if departures.length > 0}
-  <div class="flex flex-wrap gap-2 rounded bg-slate-800 p-4 opacity-60">
+  <div
+    class="flex flex-wrap gap-2 rounded bg-slate-100 p-4 opacity-60 dark:bg-slate-800 dark:text-slate-50"
+  >
     {#each departures as departure}
       {@const { relevantDate } = useTransitStopTime(departure)}
       <div class="flex items-baseline gap-1 text-sm">
