4fc1fd6e38d61ae6ac4848bb5b2b325531c65421
David Bartal <bartaldvd@gmail.com>
2024-06-10 22:29:37 +0200

    better home screen

--- src/components/StopsView.svelte
+++ src/components/StopsView.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
   import type { StopGroup } from '$lib/types';
   import SavedStopGroup from './SavedStopGroup.svelte';
+  import GondolaLift from '~icons/material-symbols/gondola-lift';
 
   let { groups }: { groups: StopGroup } = $props();
 </script>
 
 <div class="flex w-full flex-col gap-1 divide-y-2 sm:w-72">
-  {#each Object.entries(groups) as [groupType, groupItems]}
-    <SavedStopGroup {groupType} {groupItems} />
-  {:else}
-    <span class="self-center mt-6 text-slate-700 dark:text-slate-200"
-      >Stops you save will appear here</span
-    >
-  {/each}
+  {#if groups}
+    {#each Object.entries(groups) as [groupType, groupItems]}
+      <SavedStopGroup {groupType} {groupItems} />
+    {:else}
+      <div
+        class="self-center mt-4 text-slate-700 dark:border-slate-200 dark:text-slate-200 border p-8 rounded"
+      >
+        <GondolaLift class="h-24 w-24 self-center mx-auto" />
+        <div class="text-lg font-semibold text-center text-balance mt-4">
+          The stops you save will appear here
+        </div>
+        <p class="font-light text-center mt-4">Sign in to get started</p>
+      </div>
+    {/each}
+  {/if}
 </div>
