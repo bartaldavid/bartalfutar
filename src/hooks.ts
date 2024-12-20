8cc01f752771fa290d8a6873f74850dd76ea084b
Dávid Bartal <35382618+bartaldavid@users.noreply.github.com>
2024-12-20 01:01:51 +0100

    update packages, disable pwa plugin temporarily to test build

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,7 @@
 // file initialized by the Paraglide-SvelteKit CLI - Feel free to edit it
 import { i18n } from '$lib/i18n';
+import type { Transport } from '@sveltejs/kit';
 
 export const reroute = i18n.reroute();
+
+export const transport: Transport = {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,60 +1,60 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+// import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
   plugins: [
     paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),
     sveltekit(),
-    SvelteKitPWA({
-      srcDir: './src',
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'BartalFUTÁR',
-        short_name: 'BartalFUTÁR',
-        start_url: '/favorites/',
-        theme_color: '#334155',
-        icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
-        display: 'standalone',
-      },
-      filename: 'service-worker.js',
-      devOptions: {
-        enabled: true,
-        type: 'module',
-      },
-      workbox: {
-        globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
-        runtimeCaching: [
-          { urlPattern: '/favorites', handler: 'StaleWhileRevalidate' },
-        ],
-      },
-      injectRegister: null,
-    }),
+    // SvelteKitPWA({
+    //   srcDir: './src',
+    //   registerType: 'autoUpdate',
+    //   manifest: {
+    //     name: 'BartalFUTÁR',
+    //     short_name: 'BartalFUTÁR',
+    //     start_url: '/favorites/',
+    //     theme_color: '#334155',
+    //     icons: [
+    //       {
+    //         src: 'pwa-64x64.png',
+    //         sizes: '64x64',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: 'pwa-192x192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: 'pwa-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'any',
+    //       },
+    //       {
+    //         src: 'maskable-icon-512x512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //         purpose: 'maskable',
+    //       },
+    //     ],
+    //     display: 'standalone',
+    //   },
+    //   filename: 'service-worker.js',
+    //   devOptions: {
+    //     enabled: true,
+    //     type: 'module',
+    //   },
+    //   workbox: {
+    //     globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
+    //     runtimeCaching: [
+    //       { urlPattern: '/favorites', handler: 'StaleWhileRevalidate' },
+    //     ],
+    //   },
+    //   injectRegister: null,
+    // }),
     Icons({ compiler: 'svelte' }),
   ],
 });
